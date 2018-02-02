d611a0fd6ab85422d8e00e2f20317fa9e8aa9a2d
issamouu <issamouu69@gmail.com>
2018-02-02 23:31:32 +0000

    Add project creation functionality
    - Add Project class
    - Add config file with LICENSES and README template

--- project/commands/init.py
+++ project/commands/init.py
@@ -2,8 +2,12 @@
 
 
 from json import dumps
-
+from os import path,getcwd
+from .config import LICENSES, README
 from .base import Base
+import time
+from string import Template
+
 
 
 
@@ -16,6 +20,7 @@ class Init(Base):
         dict['owner_name']= input("Please enter your name: ")
         dict['owner_email']= input("Please enter your email: ")
         dict['entry_point']= input("Entrypoint file (main.py): ")
+        dict['description'] = input("Project description :  ")
         dict['license']= input("License (MIT) : ")
         dict['readme'] = input("Generate a README.md file ? (y/n)")
         return dict
@@ -23,21 +28,23 @@ class Init(Base):
     def run(self):
         init_dict = self.get_user_infos()
         project = Project(init_dict)
-        # project.make()
+        project.make()
     
 
 """ Project  class"""
 class Project():
 
     def __init__(self,init_dict):
-        self.owner_name = init_dict['owner_name'] if init_dict['owner_name'] else 'owner_name'
+        self.owner_name = init_dict['owner_name'] if init_dict['owner_name'] else '<owner_name>'
         self.owner_email = init_dict['owner_email'] if init_dict['owner_email'] else 'owner_email'
-        
+        self.project_name = path.basename(getcwd())
+        self.description = init_dict['description']
+
         self.entry_point = "main.py"
         if init_dict['entry_point'] and re.match(r'.+\.py$',init_dict['entry_point']):
             self.entry_point = init_dict['entry_point']
         
-        self.entry_point = "mit"
+        self.license = "mit"
         if init_dict['license'] and init_dict['license'].lower() in LICENSES :
             self.license = init_dict['license']
         
@@ -46,4 +53,22 @@ class Project():
             self.readme = True
 
     def make(self):
-        pass
+        # generate license file
+        s = Template(LICENSES[self.license]) 
+        #replace fields 
+        year = time.strftime("%Y")
+        license = s.safe_substitute(year=year, name=self.owner_name)
+        
+        #open new license file and write result
+        with open('LICENSE', 'w') as f:
+            f.write(license)
+        
+        #generate readme boilerplate
+        readme = Template(README).safe_substitute(project_name=self.project_name, project_description=self.description)
+        #open new license file and write result
+        with open('README.md', 'w') as f:
+            f.write(readme)
+        
+        
+
+        
